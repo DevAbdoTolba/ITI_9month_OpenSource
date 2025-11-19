0b07bd23241c2e23efeee59c49a92643feccc69d
DevAbdoTolba <DevAbdoTolba@gmail.com>
2025-11-19 21:23:20 +0200

    19 nov OOP

---
+++ cpp/oop/Day4/classes/Inheritance/Creatures.h
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+class Creatures{
+    public:
+    string names;
+    Creatures(string _name) : names(_name){}
+
+    void setNames(string _names){
+         names = _names;
+    }
+
+};
