cabf537e9d679b0edcf07ab31597d53f330a6173
DevAbdoTolba <DevAbdoTolba@gmail.com>
2025-12-29 12:33:06 +0200

    day 2 task

---
+++ js/day2/task/1.js
@@ -0,0 +1,14 @@
+let array = [
+  "JS good with you",
+  "JS good with me",
+  "JS good with us",
+  "JS good with we",
+  "JS good with they",
+  "JS good with du",
+  "JS good with er",
+  "JS good with sie",
+  "JS good with unser",
+  "JS good with ich",
+];
+
+alert(array[parseInt(Math.random() * 10)]);
