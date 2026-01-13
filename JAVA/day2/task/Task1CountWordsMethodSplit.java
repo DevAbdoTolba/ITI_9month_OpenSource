cd8d214dc1adacc7cb7315bd7d079bbcf8841fcd
DevAbdoTolba <DevAbdoTolba@gmail.com>
2026-01-13 09:54:54 +0200

    some small tweeks :)

--- JAVA/day2/task/Task1CountWordsMethodSplit.java
+++ JAVA/day2/task/Task1CountWordsMethodSplit.java
@@ -1,17 +1,21 @@
 public class T1CountWordsMethod2{
     public static void main(String[] args){
+        String word,text;
         if(args.length == 0){
-            System.out.println("No input provided.");
+            String word = "ITI";
+            String text = "ITI develops people and ITI house of developers and ITI for";
+            text = " " + text + " ";
+            System.out.println((text.split(word).length) - 1);
         } else if(args.length == 1){
             System.out.println("Please provide both the word and the text.");
         } else {
-
             String word = args[0];
             String text = args[1];
-            text = " " + text + " ";
-            System.out.println((text.split(word).length) - 1);
-
         }
+        
+        text = " " + text + " ";
+        System.out.println((text.split(word).length) - 1);
+
     }
 }
 
--- JAVA/day2/task/Task1CountWordsMethodSubString.java
+++ JAVA/day2/task/Task1CountWordsMethodSubString.java
@@ -1,21 +1,23 @@
 public class T1CountWordsMethod1{
     public static void main(String[] args){
+        String word,text;
         if(args.length == 0){
-            System.out.println("No input provided.");
+            String word = "ITI";
+            String text = "ITI develops people and ITI house of developers and ITI for";
+            text = " " + text + " ";
+            System.out.println((text.split(word).length) - 1);
         } else if(args.length == 1){
             System.out.println("Please provide both the word and the text.");
         } else {
-
             String word = args[0];
-            int count = 0;
             String text = args[1];
-
-            while(text.contains(word)){
-                count++;
-                text = text.substring(text.indexOf(word) + word.length());
-            }
-            System.out.println(count);
         }
+        
+        while(text.contains(word)){
+            count++;
+            text = text.substring(text.indexOf(word) + word.length());
+        }
+        System.out.println(count);
     }
 }
 
