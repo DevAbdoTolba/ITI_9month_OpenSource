6e4fd5d28b50ec9e5ee8e1470efa6f66dcfb3a40
DevAbdoTolba <DevAbdoTolba@gmail.com>
2026-01-13 09:54:54 +0200

    some small tweeks :)

--- JAVA/day2/task/Task2TokienizerDelimeter.java
+++ JAVA/day2/task/Task2TokienizerDelimeter.java
@@ -2,19 +2,18 @@ import java.util.StringTokenizer;
 
 public class T2TokienizerDelimeter{
     public static void main(String[] args){
+        StringTokenizer text;
         if(args.length == 0){
-            System.out.println("No input provided.");
+            text = new StringTokenizer("ITI develops people and ITI house of developers and ITI for people", "ITI");
         } else if(args.length == 1){
             System.out.println("Please provide both the word and the text.");
         } else {
-
-            StringTokenizer text = new StringTokenizer(args[1], args[0]);
-
+            text = new StringTokenizer(args[1], args[0]);
+        }
             int T = text.countTokens();
             while(T-- > 0){
                 System.out.println(text.nextToken());
             }
-        }
     }
 }
 
