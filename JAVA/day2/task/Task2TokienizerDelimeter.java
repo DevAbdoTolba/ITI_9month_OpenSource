f2dd4bb582a874340616dffcd97f9cfa1012398a
DevAbdoTolba <DevAbdoTolba@gmail.com>
2026-01-13 10:33:58 +0200

    last edit i think...

--- JAVA/day2/task/Task2TokienizerDelimeter.java
+++ JAVA/day2/task/Task2TokienizerDelimeter.java
@@ -1,21 +1,23 @@
 import java.util.StringTokenizer;
 
-public class T2TokienizerDelimeter{
+public class Task2TokienizerDelimeter{
     public static void main(String[] args){
-        StringTokenizer text;
+        StringTokenizer tokened = null;
         if(args.length == 0){
-            text = new StringTokenizer("ITI develops people and ITI house of developers and ITI for people", "ITI");
+            String word = " ";
+            String text = "ITI develops people and ITI house of developers and ITI for";
+            tokened = new StringTokenizer(text, word);
         } else if(args.length == 1){
             System.out.println("Please provide both the word and the text.");
         } else {
-            text = new StringTokenizer(args[1], args[0]);
+            tokened = new StringTokenizer(args[1], args[0]);
         }
-            int T = text.countTokens();
+            int T = tokened.countTokens();
             while(T-- > 0){
-                System.out.println(text.nextToken());
+                System.out.println(tokened.nextToken());
             }
     }
 }
 
-// javac T2TokienizerDelimeter.java && java T2TokienizerDelimeter : "hello:itsme:123123::asdasd"
-// javac T2TokienizerDelimeter.java && java T2TokienizerDelimeter " " "ITI develops people and ITI house of developers and ITI for people"
+// javac Task2TokienizerDelimeter.java && java Task2TokienizerDelimeter : "hello:itsme:123123::asdasd"
+// javac Task2TokienizerDelimeter.java && java Task2TokienizerDelimeter " " "ITI develops people and ITI house of developers and ITI for people"
