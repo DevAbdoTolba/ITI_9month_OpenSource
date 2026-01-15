d0bf1f9cf816adc4a18cb7578c70a02d3d0a7bcf
DevAbdoTolba <DevAbdoTolba@gmail.com>
2026-01-15 14:44:17 +0200

    not done day 4 tasks!

---
+++ JAVA/day4/Main.java
@@ -0,0 +1,7 @@
+public class Main{
+    public static void main(String[] args){
+        // System.out.println(()-> 1 && 2 ); // ERROR... Does not work
+        // System.out.println((()-> 2)); // ERROR... Does not work
+        
+    }
+}
