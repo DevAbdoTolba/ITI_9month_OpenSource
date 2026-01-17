d14536196c260036bc468a328f3360c789ff0d1d
Abdo Tolba <DevAbdoTolba@gmail.com>
2026-01-17 22:19:23 +0200

    day 6 task 2 contientnt done i think

--- JAVA/day6/task/highestPopulatedContinent.java
+++ JAVA/day6/task/highestPopulatedContinent.java
@@ -1,5 +1,3 @@
-
-
 import java.util.Comparator;
 import java.util.Map;
 import java.util.Optional;
@@ -7,21 +5,18 @@ import java.util.function.BiConsumer;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
-
-
 public class highestPopulatedContinent {
 
     public static void main(String[] args) {
         CountryDao countryDao = InMemoryWorldDao.getInstance();
-        countryDao.getAllContinents().stream().peek(continent -> 
-            System.out.print(continent + " : "))
-            countryDao.findCountriesByContinent(continent).stream()
-                    .flatMap(country -> country.getCities().stream())
-                    .mapToInt(city -> city.getPopulation())
-                    .max()
-                    .orElse(0)
-            );
-        );
-    }
 
+        countryDao.getAllContinents().stream()
+                .peek(continent -> System.out.print(continent + " : "))
+                .map(continent -> countryDao.findCountriesByContinent(continent).stream()
+                        .flatMap(country -> country.getCities().stream())
+                        .mapToInt(city -> city.getPopulation())
+                        .max()
+                        .orElse(0))
+                .forEach(population -> System.out.println(population));
+    }
 }
