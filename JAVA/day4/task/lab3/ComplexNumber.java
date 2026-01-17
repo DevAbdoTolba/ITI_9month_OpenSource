bfd8f6c3555e6e0fa9823db760e6c9049a109c90
DevAbdoTolba <DevAbdoTolba@gmail.com>
2026-01-17 11:30:08 +0200

    Day 4 done!

--- JAVA/day4/task/lab3/ComplexNumber.java
+++ JAVA/day4/task/lab3/ComplexNumber.java
@@ -1,54 +1,56 @@
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class ComplexNumber<T extends Number> {
-    Double real;
-    Double imaginary;
+    BigDecimal real;
+    BigDecimal imaginary;
 
     public static void main(String[] args) {
         ComplexNumber<Integer> cmpx = new ComplexNumber<>(1, 3);
-        System.out.println(cmpx.sub(new ComplexNumber<>(1, 1)));
+        System.out.println(cmpx);
 
         ComplexNumber<Double> cmpx2 = new ComplexNumber<>(1.5, 3.5);
         System.out.println(cmpx2.add(new ComplexNumber<>(1.5, 1.5)));
+        System.out.println(cmpx2.mul(new ComplexNumber<>(1.5, 1.5)));
+        System.out.println(cmpx2.sub(new ComplexNumber<>(1.5, 1.5)));
+        System.out.println(cmpx2.div(new ComplexNumber<>(1.5, 1.5)));
     }
 
-    public ComplexNumber(Double real, Double imaginary) {
-        this.real = real;
-        this.imaginary = imaginary;
-    }
-    
     public ComplexNumber(T real, T imaginary) {
-        this(real.doubleValue(), imaginary.doubleValue());
+        this.real = new BigDecimal(real.toString());
+        this.imaginary = new BigDecimal(imaginary.toString());
     }
 
-    ComplexNumber<T> add(ComplexNumber<T> incomingComplexNumber) {
+    ComplexNumber<BigDecimal> add(ComplexNumber<T> incomingComplexNumber) {
         return new ComplexNumber<>(
-            this.real + incomingComplexNumber.real,
-            this.imaginary + incomingComplexNumber.imaginary
+            this.real.add(incomingComplexNumber.real), 
+            this.imaginary.add(incomingComplexNumber.imaginary)
         );
     }
 
-    ComplexNumber<T> sub(ComplexNumber<T> incomingComplexNumber) {
+    ComplexNumber<BigDecimal> sub(ComplexNumber<T> incomingComplexNumber) {
         return new ComplexNumber<>(
-            this.real - incomingComplexNumber.real,
-            this.imaginary - incomingComplexNumber.imaginary
+            this.real.subtract(incomingComplexNumber.real), 
+            this.imaginary.subtract(incomingComplexNumber.imaginary)
         );
     }
 
-    ComplexNumber<T> mul(ComplexNumber<T> incomingComplexNumber) {
+    ComplexNumber<BigDecimal> mul(ComplexNumber<T> incomingComplexNumber) {
         return new ComplexNumber<>(
-            this.real * incomingComplexNumber.real,
-            this.imaginary * incomingComplexNumber.imaginary
+            this.real.multiply(incomingComplexNumber.real), 
+            this.imaginary.multiply(incomingComplexNumber.imaginary)
         );
     }
 
-    ComplexNumber<T> div(ComplexNumber<T> incomingComplexNumber) {
+    ComplexNumber<BigDecimal> div(ComplexNumber<T> incomingComplexNumber) {
         return new ComplexNumber<>(
-            this.real / incomingComplexNumber.real,
-            this.imaginary / incomingComplexNumber.imaginary
+            this.real.divide(incomingComplexNumber.real, RoundingMode.HALF_UP), 
+            this.imaginary.divide(incomingComplexNumber.imaginary, RoundingMode.HALF_UP)
         );
     }
 
     @Override
     public String toString() {
         return this.real + " + " + this.imaginary + "i";
-    }   
+    }
 }
