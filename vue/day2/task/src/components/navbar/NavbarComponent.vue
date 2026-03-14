716de3d2f48df4b005a92e393f37b759abfb2926
Abdo Tolba <DevAbdoTolba@gmail.com>
2026-03-14 13:43:35 +0200

    Done day 2 vue task

---
+++ vue/day2/task/src/components/navbar/NavbarComponent.vue
@@ -0,0 +1,44 @@
+<script setup>
+import { onMounted } from 'vue';
+
+const props = defineProps({
+    logo_text: String,
+    links: Array
+})
+
+onMounted( () => {
+    console.log(props.links);
+});
+</script>
+
+<template>
+    <div class="app-navbar">
+        <h2>{{props.logo_text}}</h2>
+        <ul>
+            <li v-for="link in props.links">
+                <RouterLink :to="'/'+link">{{link.toUpperCase()}}</RouterLink>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<style scoped>
+.app-navbar {
+    background: #333;
+    color: white;
+    padding: 10px;
+    margin-bottom: 20px;
+}
+.app-navbar ul {
+    list-style: none;
+    padding: 0;
+}
+.app-navbar li {
+    display: inline;
+    margin-right: 15px;
+}
+.app-navbar a {
+    color: yellow;
+    text-decoration: none;
+}
+</style>
---
+++ vue/day2/task/src/views/HomeView.vue
@@ -0,0 +1,28 @@
+<script setup>
+import MainProduct from '@/components/product/MainProduct.vue'
+import RelatedProducts from '@/components/product/RelatedProducts.vue'
+
+const props = defineProps({
+    main_product: Object,
+    products: []
+})
+
+
+    console.log("HEEHRHEEEEEE:", props.main_product);
+    console.log("After HEEHRHEEEEEE:", props.products);
+
+const emit = defineEmits(['buy-emit'])
+
+function buyMainProduct() {
+    emit('buy-emit');
+}
+</script>
+
+<template>
+  <MainProduct 
+    :main_product="main_product"
+    @buy-product="buyMainProduct"
+  />
+
+  <RelatedProducts :products="products"/>
+</template>
